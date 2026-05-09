4c57f1e3a529529046a6bdff73f4c93ad5bd374c
penchuks <penchuksen@gmail.com>
2026-05-09 10:38:57 +0100

    Fix: remove onchainkit from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { OnchainKitProvider } from '@coinbase/onchainkit'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider, createConfig, http } from 'wagmi'
 import { baseSepolia } from 'wagmi/chains'
@@ -18,9 +17,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <OnchainKitProvider apiKey={import.meta.env.VITE_COINBASE_API_KEY} chain={baseSepolia}>
-          <App />
-        </OnchainKitProvider>
+        <App />
       </QueryClientProvider>
     </WagmiProvider>
   </StrictMode>
